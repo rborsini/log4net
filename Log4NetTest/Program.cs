5ce86f2a54c5763e3a7709b5a49141105d53737d
r.borsini <roberto.borsini@gmail.com>
2015-09-09 10:01:31 +0200

    Esempio per limitare la dimensione massima della cartella di log

---
+++ Log4NetTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Log4NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            log4net.Config.XmlConfigurator.Configure();
+
+            log4net.ILog log = log4net.LogManager.GetLogger("Test");
+
+            while(true)
+            {
+                log.Info("AAA");
+                Thread.Sleep(10);
+            }
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
